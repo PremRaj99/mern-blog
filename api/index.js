28d8c52561de1876e535d246fac2cd7cf65cc9b8
Prem Raj <premraj299341@gmail.com>
2024-02-02 23:31:22 +0530

    connect to the DataBase MongoDb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("MongoDb is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen('80', () => {
-    console.log("server is running on port 80")
+app.listen("80", () => {
+  console.log("server is running on port 80");
 });
