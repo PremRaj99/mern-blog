2f612664959f7fa805db1a096ef074fa349d3512
Prem Raj <premraj299341@gmail.com>
2024-02-28 17:29:17 +0530

    add delete functionallity to the comment section

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -78,7 +78,25 @@ export const editComment = async (req, res, next) => {
       },
       { new: true }
     );
-    res.status(200).json(editComment)
+    res.status(200).json(editComment);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not allowed to delete this comment")
+      );
+    }
+    await Comment.findByIdAndDelete(req.params.commentId);
+    res.status(200).json("Commnet has been deleted");
   } catch (error) {
     return next(error);
   }
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -38,14 +38,13 @@ export default function Comment({ comment, onLike, onEdit }) {
         },
         body: JSON.stringify({ content: editedContent }),
       });
-      if(res.ok) {
-        setIsEditing(false)
-        onEdit(comment, editedContent)
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent);
         const data = await res.json();
-
       }
     } catch (error) {
-      setEditedContent(false)
+      setEditedContent(false);
       alert(error.message);
     }
   };
@@ -117,13 +116,22 @@ export default function Comment({ comment, onLike, onEdit }) {
               </p>
               {currentUser &&
                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button
-                    type="button"
-                    className="text-gray-400 hover:text-blue-500"
-                    onClick={handleEdit}
-                  >
-                    Edit
-                  </button>
+                  <>
+                    <button
+                      type="button"
+                      className="text-gray-400 hover:text-blue-500"
+                      onClick={handleEdit}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="text-gray-400 hover:text-red-500"
+                      onClick={() => onDelete(comment._id)}
+                    >
+                      Delete
+                    </button>
+                  </>
                 )}
             </div>
           </>
