b7b54e19a356a252e4a41941c0d1b226ef6f8e0e
Prem Raj <premraj299341@gmail.com>
2024-03-02 21:35:02 +0530

    fix a small bug for render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import DashBoard from "./pages/DashBoard";
 import Projects from "./pages/Projects";
 import SignIn from "./pages/Signin";
